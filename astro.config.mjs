0764e8efd822e8c2f331e280565fda34423d5713
Krystian G <144816009+vertira@users.noreply.github.com>
2024-05-30 23:01:37 +0200

    Revert "fixing...."
    This reverts commit d8b06e4a767fdfe33f12b87b77eabd8c43706b87.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
-import playformCompress from "@playform/compress";
-
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react(), playformCompress()],
+	integrations: [
+		tailwind(),
+		react(),
+		(await import("@playform/compress")).default(),
+	],
 	output: "server",
 	adapter: vercel(),
 });
