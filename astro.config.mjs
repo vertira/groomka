386be12e1644cbeaf8d9bc000087243515cc2715
Krystian G <144816009+vertira@users.noreply.github.com>
2024-05-30 22:58:29 +0200

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
-import playformCompress from "@playform/compress";
-
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react(), playformCompress()],
+	integrations: [tailwind(), react()],
 	output: "server",
 	adapter: vercel(),
 });
