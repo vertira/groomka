a1cd64f49fd816865a10b8333a01d9ac4f59a37f
Krystian G <144816009+vertira@users.noreply.github.com>
2024-05-31 01:07:24 +0200

    lottie after 0.3 s

--- src/components/Lottie.tsx
+++ src/components/Lottie.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
 interface LottieProps {
-	src: string;
-	styles: string;
+    src: string;
+    styles: string;
 }
+
 const Lottie = ({ src, styles }: LottieProps) => {
-	return (
-		<>
-			<DotLottieReact src={src} loop autoplay className={styles} />
-		</>
-	);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoaded(true);
+        }, 300); // Opóźnienie 3 sekundy
+
+        return () => clearTimeout(timer); // Czysta funkcja, która usuwa timer w przypadku odmontowania komponentu
+    }, []);
+
+    return (
+        <>
+            {isLoaded && <DotLottieReact src={src} loop autoplay className={styles} />}
+        </>
+    );
 };
 
 export default Lottie;
