d8b06e4a767fdfe33f12b87b77eabd8c43706b87
Krystian G <144816009+vertira@users.noreply.github.com>
2024-05-30 22:52:14 +0200

    fixing....

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		react(),
-		(await import("@playform/compress")).default(),
-	],
+	integrations: [tailwind(), react(), playformCompress()],
 	output: "server",
 	adapter: vercel(),
 });
