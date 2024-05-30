2d84a606c13955d67ac84d4d0026c0bbe68b0ff5
Krystian G <144816009+vertira@users.noreply.github.com>
2024-05-30 23:01:44 +0200

    Revert "fixing...."
    This reverts commit 7a26b66a7f6db78c9169d4053508f5a39780d826.

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_BjMP1uev.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_DK0M6cND.mjs');
-
-export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
+
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		react(),
-		(await import("@playform/compress")).default(),
-	],
-	output: "server",
-	adapter: vercel(),
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
