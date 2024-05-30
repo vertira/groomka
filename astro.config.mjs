3fd290365902d90afadac0c0d059ee8c350b2da3
Krystian G <144816009+vertira@users.noreply.github.com>
2024-05-30 23:08:26 +0200

    trying...

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), react()],
-	output: "hybrid",
+	output: "static",
 	adapter: vercel(),
 });
