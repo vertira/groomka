1a37177ca211f8ffc2440d2085519b637f186bd2
Krystian G <144816009+vertira@users.noreply.github.com>
2024-05-31 02:12:40 +0200

    update animations

--- src/components/Motion.tsx
+++ src/components/Motion.tsx
@@ -5,17 +5,24 @@ import { motion } from "framer-motion";
 interface AnimatedDivProps {
 	children: React.ReactNode;
 	className?: string;
-	ystart?: number;
+	rotate?: number;
 	scale?: number;
+	duration?: number;
 }
 
-const Motion = ({ children, className, ystart, scale }: AnimatedDivProps) => {
+const Motion = ({
+	children,
+	className,
+	rotate,
+	scale,
+	duration,
+}: AnimatedDivProps) => {
 	return (
 		<motion.div
 			className={className}
-			initial={{ opacity: 0, y: ystart, scale: scale }}
-			whileInView={{ opacity: 1, y: 0, scale: 1 }}
-			transition={{ duration: 1 }}
+			initial={{ opacity: 0, rotate: rotate, scale: scale }}
+			whileInView={{ opacity: 1, rotate: 0, scale: 1 }}
+			transition={{ duration: duration }}
 		>
 			{children}
 		</motion.div>
