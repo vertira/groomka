81758173f8a6465609a2840bf645e5abb63fba63
Krystian G <144816009+vertira@users.noreply.github.com>
2024-08-12 12:05:26 +0200

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="astro/client" />
-declare namespace App {
-    interface Locals {
-      user?:
-        | import("node-appwrite").Models.User<
-            import("node-appwrite").Models.Preferences<{}>
-          >;
-    }
-  }
+// declare namespace App {
+//     interface Locals {
+//       user?:
+//         | import("node-appwrite").Models.User<
+//             import("node-appwrite").Models.Preferences<{}>
+//           >;
+//     }
+//   }
