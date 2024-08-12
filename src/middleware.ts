8787e6799a5299b7c8b7db9dc84ae9e43aa81284
Krystian G <144816009+vertira@users.noreply.github.com>
2024-08-12 12:02:23 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-import { defineMiddleware } from "astro:middleware";
-import { account } from "./lib/appwrite";
+// import { defineMiddleware } from "astro:middleware";
+// import { account } from "./lib/appwrite";
 
-export const onRequest = defineMiddleware(async ({ request, locals }, next) => {
-  try {
-    locals.user = await account.get();
-  } catch {
-    return null
-  }
+// export const onRequest = defineMiddleware(async ({ request, locals }, next) => {
+//   try {
+//     locals.user = await account.get();
+//   } catch {
+//     return null
+//   }
 
-  return next();
-});
+//   return next();
+// });
